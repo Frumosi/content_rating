68fc7fcdb579cce627ac56409026d6ebfd43271b
Frumosi <kkone.devops@gmail.com>
2024-10-20 07:45:32 -0400

    Updated

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -8,6 +8,7 @@ class ContentRating extends Component {
     this.state = {
         likes: 0,
         dislikes: 0,
+        totalRatings: 0,
         handleLike:() => {
             this.setState((prevState) => ({
                 likes: prevState.likes + 1,
@@ -41,6 +42,7 @@ class ContentRating extends Component {
             Dislike ({this.state.dislikes})
           </button>
         </div>
+        <p>Total Ratings: {this.state.totalRatings}</p>
 
      </div>
      </>
